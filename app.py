b61bf76364a3a4a06b95a5f04ae177fc61f81e3d
montamba <monmon272005@gmail.com>
2026-03-20 19:26:02 +0800

    hello

--- app.py
+++ app.py
@@ -1,30 +1,18 @@
 from flask import Flask, request
-from twilio.twiml.voice_response import VoiceResponse
+from twilio.twiml.voice_response import VoiceResponse, Gather
 
 app = Flask(__name__)
 
-@app.route("/voice", methods=["POST"])
-def voice():
-    response = VoiceResponse()
+@app.route("/call", methods=["GET", "POST"])
+def call():
+    res = VoiceResponse()
+    gather = Gather(input="speech")
+    gather.say("Hello, this is a test")
+    res.append(gather)
+    return str(res)
 
-    gather = response.gather(
-        input="speech",
-        action="/process",
-        speechTimeout="auto"
-    )
-
-    gather.say("Hello, what is your question?")
-
-    return str(response)
-
-
-@app.route("/process", methods=["POST"])
-def process():
-    speech = request.form.get("SpeechResult")
-
-    print("Caller said:", speech)
-
-    response = VoiceResponse()
-    response.say(f"You said {speech}")
-
-    return str(response)
+@app.route("/hello")
+def hello():
+    return "Helllo po"
+if __name__ == "__main__":
+    app.run(port=80)
