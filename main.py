51a9b7aea9db4010f87da0ba1a0948c763796eca
montamba <monmon272005@gmail.com>
2026-03-20 20:03:21 +0800

    hello

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from twilio.twiml.voice_response import VoiceResponse, Gather
-
+import os
 app = Flask(__name__)
 
 @app.route("/call", methods=["GET", "POST"])
@@ -14,3 +14,9 @@ def call():
 @app.route("/hello")
 def hello():
     return "Helllo po"
+
+
+if __name__ == "__main__":
+    # Railway provides the PORT in an environment variable
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
